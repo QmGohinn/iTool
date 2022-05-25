246fc7cc7d81f406af032ca52dbecb6b5a41a8cc
qm <1559874707@qq.com>
2022-05-25 10:13:41 +0800

    第一个默认窗体

---
+++ _BASE/main.cpp
@@ -0,0 +1,11 @@
+#include <_APL/iToolMainForm/mainwindow.h>
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
